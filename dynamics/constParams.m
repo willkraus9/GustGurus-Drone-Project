0bfccb2930aa453477d0ca3242c850cb3c25611b
DenisAlpay <150480897+DenisAlpay@users.noreply.github.com>
2024-11-21 12:05:33 -0500

    Made constParams a struct

--- dynamics/constParams.m
+++ dynamics/constParams.m
@@ -1,19 +1,26 @@
 clear;
 
-m=0.33;  %kg
-d= 39.73e-3;  % m arm length
-r=23.1348;  % rotor radius %m
-Ix=1.395e-5;  % kg*m2
-Iy=1.436e-5;  % kg*m2
-Iz=2.173e-5;  % kg*m2
-I_B = diag([Ix,Iy,Iz]);  % kg*m2
-kT=0.2025;  % Thrust coefficient
-kD=0.11;  % torque coefficient
-Cd=7.9379e-12;  
-Ct=3.1582e-10;  
-Ctbar=d*Ct/sqrt(2);
-TW=[Ct, Ct, Ct, Ct; -Ctbar, -Ctbar, Ctbar, Ctbar; -Ctbar, Ctbar, Ctbar, -Ctbar; -Cd, Cd, -Cd, Cd];
-g=9.81;
-Ax=0.35;
-Ay=Ax;
-Az=Ax;
+params = struct();
+
+params.m=0.33;  %kg
+params.d= 39.73e-3;  % m arm length
+params.r=23.1348;  % rotor radius %m
+params.Ix=1.395e-5;  % kg*m2
+params.Iy=1.436e-5;  % kg*m2
+params.Iz=2.173e-5;  % kg*m2
+params.I_B = diag([params.Ix, params.Iy, params.Iz]);  % kg*m2
+params.kT=0.2025;  % Thrust coefficient
+params.kD=0.11;  % torque coefficient
+params.Cd=7.9379e-12;  
+params.Ct=3.1582e-10;  
+params.Ctbar=params.d*params.Ct/sqrt(2);
+
+params.TW=[params.Ct, params.Ct, params.Ct, params.Ct;
+           -params.Ctbar, -params.Ctbar, params.Ctbar, params.Ctbar; 
+           -params.Ctbar, params.Ctbar, params.Ctbar, -params.Ctbar; 
+           -params.Cd, params.Cd, -params.Cd, params.Cd];
+
+params.g=9.81;
+params.Ax=0.35;
+params.Ay=params.Ax;
+params.Az=params.Ax;

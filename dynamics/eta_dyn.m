a3154377d77d9a0814b5c6176d9a06014d9acac4
DenisAlpay <150480897+DenisAlpay@users.noreply.github.com>
2024-11-21 11:38:40 -0500

    started simulink model

--- dynamics/eta_dyn.m
+++ dynamics/eta_dyn.m
@@ -1,8 +1,8 @@
-function etadd = eta_dyn(eta, eta_d, tau)
+function eta_dd = eta_dyn(eta, eta_d, tau)
 %ETA_DYN Summary of this function goes here
 %   Detailed explanation goes here
-J=inertia_matrix_J(eta,I_B);
-C = Coriolis(eta,eta_d);
-etadd=inv(J)*(tau-C*eta_d);
+    J=inertia_matrix_J(eta,I_B);
+    C = Coriolis(eta,eta_d);
+    eta_dd=inv(J)*(tau-C*eta_d);
 end
 

09dbd09e006603327a416497cf07772a7b389e27
DenisAlpay <150480897+DenisAlpay@users.noreply.github.com>
2024-11-22 18:47:26 -0500

    added crazyflie-firmware

---
+++ crazyflie-firmware/docs/building-and-flashing/build.md
@@ -0,0 +1,272 @@
+---
+title: Building and Flashing
+page_id: build
+---
+
+## Dependencies
+
+You'll need to use either the [Crazyflie VM](https://github.com/bitcraze/bitcraze-vm),
+[the toolbelt](https://github.com/bitcraze/toolbelt) or
+install some ARM toolchain.
+
+### Install a toolchain
+
+#### Toolchain and compiler version policy
+Our toolchain policy is to support the two most recent [Ubuntu Long Term Support (LTS) releases](https://wiki.ubuntu.com/Releases). This ensures we stay aligned with widely used, actively maintained versions. As of October 15th, 2024, the oldest supported LTS release is Ubuntu 22.04 (Jammy Jellyfish), which includes `gcc-arm-none-eabi` version 10.3.
+
+This means that if the firmware can not be compiled using gcc 10.3, **or anything newer**, it should be considered a bug.
+
+##### Ubuntu
+```
+$ sudo apt-get install make gcc-arm-none-eabi
+```
+
+##### macOS
+```
+$ brew tap PX4/homebrew-px4
+$ brew install gcc-arm-none-eabi
+```
+
+##### Windows
+The supported way to build the Crazyflie on Windows is to use the Windows Subsystem for Linux (WSL) on Windows 10+.
+This means that developement happens in a Linux environment.
+Flashing is handled by installing Python and the Crazyflie client on Windows launched from linux.
+
+To get started you need to [enable WSL and install an Ubuntu system](https://docs.microsoft.com/en-us/windows/wsl/install).
+This can be done by opening `power shell` as administrator and typing:
+
+```
+$ wsl --install
+```
+
+Then follow the [install instruction for Ubuntu 20.04](#ubuntu) above to install the required build dependencies.
+
+For [flashing](#flashing) you need to install [Python](https://www.python.org/downloads/windows/) (=>version 3.7) and the [CFclient](https://github.com/bitcraze/crazyflie-clients-python) **on Windows**.
+When installing Python, the checkbox to add python to the Path should be checked and then the CFclient can be installed with pip in a `powershell` or `cmd` window:
+```
+$ pip.exe install cfclient
+```
+
+The Crazyflie makefile will automatically use the Windows python when running in WSL.
+
+### Cloning
+
+This repository uses git submodules. Clone with the `--recursive` flag
+
+```
+$ git clone --recursive https://github.com/bitcraze/crazyflie-firmware.git
+```
+
+> Note: Make sure there are no spaces in the folder structure leading up to the repository (example: _/a/path/with/no/spaces/crazyflie-firmware/_ vs _a/path with spaces/crazyflie-firmware/_). Our build system can not handle file system paths with spaces in them, and compilation will fail.
+
+If you already have cloned the repo without the `--recursive` option, you need to
+get the submodules manually
+
+```
+$ cd crazyflie-firmware
+$ git submodule init
+$ git submodule update
+```
+
+
+## Compiling
+
+### Configuration
+
+Before you can build the firmware, you will need to configure it. To use a platform default configuration, run the following command:
+
+#### Crazyflie 2.0, Crazyflie 2.1(+)
+```
+$ make cf2_defconfig
+```
+#### Crazyflie 2.1 Brushless
+```
+$ make cf21bl_defconfig
+```
+#### Crazyflie Bolt
+```
+$ make bolt_defconfig
+```
+
+### Building the firmware
+Then build the firmware with:
+
+#### Linux/WSL
+```
+$ make -j$(nproc)
+```
+#### macOS
+```
+$ make -j$(sysctl -n hw.ncpu)
+```
+
+>  Alternatively, to configure and build with the toolbelt, prepend `tb` to the make commands as follows:
+>
+>  ```
+>  $ tb make cf2_defconfig
+>  $ tb make
+>  ```
+
+Build artifacts, including binaries, will end up in the `build` directory.
+
+### Customize the firmware with kbuild (Advanced)
+
+
+**Please note** that these instructions are only meant for you if you want to build an custom firmware for a custom platform than the ones we have listed above. You can still configure and change the firmware without kbuild.
+
+Please go to [these instructions](/docs/development/kbuild.md) to learn how to use the menuconfig.
+
+
+### Build python bindings
+
+There are certain functions, like the high level commander and controllers, that have been wrapped to python bindings. These can be used to easily test these functions on a computer or use it in a simulation.
+
+First make sure that you have [SWIG](https://swig.org/) installed on your system. Then execute the following commands in the terminal
+
+```
+$ make cf2_defconfig
+$ make bindings_python
+$ cd build
+$ python3 setup.py install --user
+```
+
+## Make targets
+
+### General targets
+```
+all             : Shortcut for build
+compile         : Compile cflie.hex. WARNING: Do NOT update version.c
+build           : Update version.c and compile cflie.elf/hex
+clean_o         : Clean only the Objects files, keep the executables (ie .elf, .hex)
+clean           : Clean every compiled files
+mrproper        : Clean every compiled files and the classical editors backup files
+
+cload           : If the crazyflie-clients-python is placed on the same directory level and
+             the Crazyradio/Crazyradio PA is inserted it will try to flash the firmware
+             using the wireless bootloader.
+flash           : Flash .elf using OpenOCD
+halt            : Halt the target using OpenOCD
+reset           : Reset the target using OpenOCD
+openocd         : Launch OpenOCD
+rtt             : Start RTT server. Compile the firmware with "DEBUG_PRINT_ON_SEGGER_RTT=1"
+             and the console is visible over TCP on port 2000 "telnet localhost 2000".
+bindings_python : Build the python bindings for firmware wrappers
+```
+
+### Noteable Kbuild targets
+```
+menuconfig      : Open up a terminal user interface to set configuration options
+defconfig       : Generate a `.config` with the default configuration options
+cf2_defconfig   : Merge configuration options from `configs/cf2_defconfig` with default
+cf21bl_defconfig: Merge configuration options from `configs/cf21bl_defconfig` with default
+tag_defconfig   : Merge configuration options from `configs/tag_defconfig` with default
+bolt_defconfig   : Merge configuration options from `configs/bolt_defconfig` with default
+allyesconfig    : Generate a `.config` with the all configuration options enabled
+allnoconfig     : Generate a `.config` with the all configuration options disabled
+randconfig      : Generate a `.config` with random valid values to all configuration options
+```
+
+## Flashing
+Writing a new binary to the Crazyflie is called flashing (writing it to the flash memory). This page describes how to flash from the command line and there are a few different ways to do it.
+
+### Using Crazyradio
+
+The supported way to flash when developping for the Crazyflie is to use the Crazyradio and the radio bootloader.
+
+#### Prerequisites
+* A Crazyradio with drivers installed
+* [Crazyflie Client installed](https://github.com/bitcraze/crazyflie-clients-python) with Python's pip (so not by Snap (Ubuntu) or the .exe (Windows))
+  * Note than when developping in WSL on Windows, the client needs to be installed on Windows. See the [Windows build instruction](#windows) above.
+* The firmware has been built
+* The current working directory is the root of the crazyflie-firmware project
+
+#### Manually entering bootloader mode
+
+* Turn the Crazyflie off
+* Start the Crazyflie in bootloader mode by pressing the power button for 3 seconds. Both the blue LEDs will blink.
+* In your terminal, run
+
+```
+$ make cload
+```
+
+It will try to find a Crazyflie in bootloader mode and flash the binary to it.
+
+Warning: if multiple Crazyflies within range are in bootloader mode the result is unpredictable. This method is not suitable in classroom situation where it is likely that several students are flashing at the same time. Also remember that the Crazyradio PA often reaches into the next room.
+
+#### Automatically enter bootloader mode
+* Make sure the Crazyflie is on
+* In your terminal, run `CLOAD_CMDS="-w [CRAZYFLIE_URI]" make cload`
+* or run `cfloader flash build/cf2.bin stm32-fw -w [CRAZYFLIE_URI]`
+with [CRAZYFLIE_URI] being the uri of the crazyflie.
+
+It will connect to the Crazyflie with the specified address, put it in bootloader mode and flash the binary. This method is suitable for classroom situations.
+
+Note: this method does not work if the Crazyflie does not start, for instance if the current flashed binary is corrupt. You will have to fall back to manually entering bootloader mode.
+
+### Using a debug adapter
+
+You need:
+
+* An ST Link V2 or V3 Debugger
+* open ocd installed ([installation intructions](/docs/development/openocd_gdb_debugging.md))
+* The firmware has been built
+* The current working directory is the root of the crazyflie-firmware project
+
+In your terminal, run
+
+```
+$ make flash
+```
+
+## Unit testing
+
+### Running all unit tests
+
+With the environment set up locally
+
+```
+$ make unit
+```
+
+with the docker builder image and the toolbelt
+
+```
+$ tb make unit
+```
+
+### Running one unit test
+
+When working with one specific file it is often convenient to run only one unit test
+
+```
+$ make unit FILES=test/utils/src/test_num.c
+```
+
+or with the toolbelt
+
+```
+$ tb make unit FILES=test/utils/src/test_num.c
+```
+
+### Running unit tests with specific build settings
+
+Defines are managed by make and are passed on to the unit test code. Use the
+normal ways of configuring make when running tests. For instance to run test
+for Crazyflie 1
+
+```
+$ make unit LPS_TDOA_ENABLE=1
+```
+
+### Dependencies
+
+Frameworks for unit testing and mocking are pulled in as git submodules.
+
+The testing framework uses ruby and rake to generate and run code.
+
+To minimize the need for installations and configuration, use the docker builder
+image (bitcraze/builder) that contains all tools needed. All scripts in the
+tools/build directory are intended to be run in the image. The
+[toolbelt](https://github.com/bitcraze/toolbelt) makes it
+easy to run the tool scripts.
---
+++ crazyflie-firmware/src/modules/src/planner.c
@@ -0,0 +1,370 @@
+/*
+ *    ______
+ *   / ____/________ _____  __  ________      ______ __________ ___
+ *  / /   / ___/ __ `/_  / / / / / ___/ | /| / / __ `/ ___/ __ `__ \
+ * / /___/ /  / /_/ / / /_/ /_/ (__  )| |/ |/ / /_/ / /  / / / / / /
+ * \____/_/   \__,_/ /___/\__, /____/ |__/|__/\__,_/_/  /_/ /_/ /_/
+ *                       /____/
+ *
+ * Crazyswarm advanced control firmware for Crazyflie
+ *
+
+The MIT License (MIT)
+
+Copyright (c) 2018 Wolfgang Hoenig and James Alan Preiss
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+/*
+implementation of planning state machine
+*/
+#include <stddef.h>
+#include "planner.h"
+#include "arm_math.h"
+#include "debug.h"
+
+static struct traj_eval plan_eval(struct planner *p, float t);
+
+static void plan_takeoff_or_landing(struct planner *p, struct vec curr_pos, float curr_yaw, float hover_height, float hover_yaw, float duration)
+{
+	struct vec hover_pos = curr_pos;
+	hover_pos.z = hover_height;
+
+	// compute the shortest possible rotation towards 0
+	hover_yaw = normalize_radians(hover_yaw);
+	curr_yaw = normalize_radians(curr_yaw);
+	float goal_yaw = curr_yaw + shortest_signed_angle_radians(curr_yaw, hover_yaw);
+
+	piecewise_plan_7th_order_no_jerk(&p->planned_trajectory, duration,
+		curr_pos,  curr_yaw,  vzero(), 0, vzero(),
+		hover_pos, goal_yaw, vzero(), 0, vzero());
+}
+
+// ----------------- //
+// public functions. //
+// ----------------- //
+
+void plan_init(struct planner *p)
+{
+	p->state = TRAJECTORY_STATE_IDLE;
+	p->type = TRAJECTORY_TYPE_PIECEWISE;
+	p->reversed = false;
+	p->trajectory = NULL;
+	p->compressed_trajectory = NULL;
+	p->planned_trajectory.pieces = p->pieces;
+}
+
+void plan_stop(struct planner *p)
+{
+	p->state = TRAJECTORY_STATE_IDLE;
+}
+
+bool plan_is_finished(struct planner *p, float t)
+{
+	switch (p->type) {
+		case TRAJECTORY_TYPE_PIECEWISE:
+			return piecewise_is_finished(p->trajectory, t);
+
+		case TRAJECTORY_TYPE_PIECEWISE_COMPRESSED:
+		  return piecewise_compressed_is_finished(p->compressed_trajectory, t);
+
+		default:
+		  return 1;
+	}
+}
+
+bool plan_is_stopped(struct planner *p)
+{
+	return p->state == TRAJECTORY_STATE_IDLE;
+}
+
+void plan_disable(struct planner *p)
+{
+	p->state = TRAJECTORY_STATE_DISABLED;
+}
+
+bool plan_is_disabled(struct planner *p)
+{
+	return p->state == TRAJECTORY_STATE_DISABLED;
+}
+
+struct traj_eval plan_current_goal(struct planner *p, float t)
+{
+	switch (p->state) {
+		case TRAJECTORY_STATE_LANDING:
+			if (plan_is_finished(p, t)) {
+				p->state = TRAJECTORY_STATE_IDLE;
+			}
+			// intentional fall-thru
+		case TRAJECTORY_STATE_FLYING:
+			return plan_eval(p, t);
+
+		default:
+			return traj_eval_invalid();
+	}
+}
+
+struct traj_eval plan_eval(struct planner *p, float t)
+{
+	switch (p->type) {
+		case TRAJECTORY_TYPE_PIECEWISE:
+			if (p->reversed) {
+				return piecewise_eval_reversed(p->trajectory, t);
+			}
+			else {
+				return piecewise_eval(p->trajectory, t);
+			}
+			break;
+
+		case TRAJECTORY_TYPE_PIECEWISE_COMPRESSED:
+			if (p->reversed) {
+				/* not supported */
+				return traj_eval_invalid();
+			}
+			else {
+				return piecewise_compressed_eval(p->compressed_trajectory, t);
+			}
+			break;
+
+		default:
+			return traj_eval_invalid();
+	}
+}
+
+int plan_takeoff(struct planner *p, struct vec curr_pos, float curr_yaw, float hover_height, float hover_yaw, float duration, float t)
+{
+	if (p->state != TRAJECTORY_STATE_IDLE) {
+		return 1;
+	}
+
+	plan_takeoff_or_landing(p, curr_pos, curr_yaw, hover_height, hover_yaw, duration);
+	p->reversed = false;
+	p->state = TRAJECTORY_STATE_FLYING;
+	p->type = TRAJECTORY_TYPE_PIECEWISE;
+	p->planned_trajectory.t_begin = t;
+	p->trajectory = &p->planned_trajectory;
+	return 0;
+}
+
+int plan_land(struct planner *p, struct vec curr_pos, float curr_yaw, float hover_height, float hover_yaw, float duration, float t)
+{
+	if (p->state == TRAJECTORY_STATE_LANDING) {
+		return 1;
+	}
+
+	plan_takeoff_or_landing(p, curr_pos, curr_yaw, hover_height, hover_yaw, duration);
+	p->reversed = false;
+	p->state = TRAJECTORY_STATE_LANDING;
+	p->type = TRAJECTORY_TYPE_PIECEWISE;
+	p->planned_trajectory.t_begin = t;
+	p->trajectory = &p->planned_trajectory;
+	return 0;
+}
+
+int plan_go_to_from(struct planner *p, const struct traj_eval *curr_eval, bool relative, bool linear, struct vec hover_pos, float hover_yaw, float duration, float t)
+{
+	if (relative) {
+		hover_pos = vadd(hover_pos, curr_eval->pos);
+		hover_yaw += curr_eval->yaw;
+	}
+
+	// compute the shortest possible rotation towards 0
+	float curr_yaw = normalize_radians(curr_eval->yaw);
+	hover_yaw = normalize_radians(hover_yaw);
+	float delta_yaw = shortest_signed_angle_radians(curr_yaw, hover_yaw);
+	float goal_yaw = curr_yaw + delta_yaw;
+
+	if (linear) {
+		struct vec vel = vdiv(vsub(hover_pos,curr_eval->pos), duration);
+		float omz = delta_yaw/duration;
+		
+		piecewise_plan_7th_order_no_jerk(&p->planned_trajectory, duration,
+		curr_eval->pos, curr_yaw, vel, omz, vzero(),
+		hover_pos,      goal_yaw, vel, omz, vzero());
+	}
+	else {
+		piecewise_plan_7th_order_no_jerk(&p->planned_trajectory, duration,
+		curr_eval->pos, curr_yaw, curr_eval->vel, curr_eval->omega.z, curr_eval->acc,
+		hover_pos,      goal_yaw,      vzero(),        0,                  vzero());
+	}
+
+	p->reversed = false;
+	p->state = TRAJECTORY_STATE_FLYING;
+	p->type = TRAJECTORY_TYPE_PIECEWISE;
+	p->planned_trajectory.t_begin = t;
+	p->trajectory = &p->planned_trajectory;
+	return 0;
+}
+
+int plan_go_to(struct planner *p, bool relative, bool linear, struct vec hover_pos, float hover_yaw, float duration, float t)
+{
+	struct traj_eval setpoint = plan_current_goal(p, t);
+	return plan_go_to_from(p, &setpoint, relative, linear, hover_pos, hover_yaw, duration, t);
+}
+
+int plan_spiral_from(struct planner *p, const struct traj_eval *curr_eval, bool sideways, bool clockwise, float spiral_angle, float radius0, float radiusF, float ascent, float duration, float t)
+{
+	// Limitting the inputs
+  if (spiral_angle > 2*PI) {
+    spiral_angle = 2*PI;
+    DEBUG_PRINT("Warning: spiral angle saturated at 2pi\n");
+  }
+  else if (spiral_angle < -2*PI) {
+    spiral_angle = -2*PI;
+    DEBUG_PRINT("Warning: spiral angle saturated at -2pi\n");
+  }
+  if (radius0 < 0) {
+    radius0 = 0;
+    DEBUG_PRINT("Warning: radius set to 0 (was negative) \n");
+  }
+  if (radiusF < 0) {
+    radiusF = 0;
+    DEBUG_PRINT("Warning: radius set to 0 (was negative) \n");
+  }
+    
+  int sense = -1 + 2*clockwise; // -1 counter-clockwise, +1 clockwise
+	float omz =  -sense*spiral_angle/duration;
+	float dz = ascent/duration;
+	float dr = (radiusF - radius0)/duration;
+	
+	struct vec pos0 = curr_eval->pos;
+	
+	float phi0 = normalize_radians(curr_eval->yaw);
+	
+	float yawF = phi0 - sense*spiral_angle;
+	float phiF = phi0 - sense*spiral_angle;
+
+	float c0 = cosf(phi0);
+	float s0 = sinf(phi0);
+	float cF = cosf(phiF);
+	float sF = sinf(phiF);
+
+	float z0 = pos0.z;
+	float xCenter, yCenter, dx0, ddx0, xF, dxF, ddxF, dy0, ddy0, yF, dyF, ddyF;
+
+	if (sideways)
+	{
+		// center position
+		xCenter = pos0.x + sense*radius0*c0;
+		yCenter = pos0.y + sense*radius0*s0;
+		
+		// x0 = -sense*radius0*c0;
+		dx0 = sense*(radius0*s0*omz - dr*c0);
+		ddx0 = sense*(radius0*c0*omz*omz + 2*dr*s0*omz);
+
+		xF = -sense*radiusF*cF;
+		dxF = sense*(radiusF*sF*omz - dr*cF);
+		ddxF = sense*(radiusF*cF*omz*omz + 2*dr*sF*omz);
+		
+		// y0 = -sense*radius0*s0;
+		dy0 = sense*(-radius0*c0*omz - dr*s0);
+		ddy0 = sense*(radius0*s0*omz*omz - 2*dr*c0*omz);
+
+		yF = -sense*radiusF*sF;
+		dyF = sense*(-radiusF*cF*omz - dr*sF);
+		ddyF = sense*(radiusF*sF*omz*omz - 2*dr*cF*omz);
+	}
+	else
+	{
+		// center position
+		xCenter = pos0.x + sense*radius0*s0;
+		yCenter = pos0.y - sense*radius0*c0;
+		
+		// x0 = -sense*radius0*s0;
+		dx0 = -sense*(radius0*c0*omz + dr*s0);
+		ddx0 = sense*(radius0*s0*omz*omz - 2*dr*c0*omz);
+
+		xF = -sense*radiusF*sF;
+		dxF = -sense*(radiusF*cF*omz + dr*sF);
+		ddxF = sense*(radiusF*sF*omz*omz - 2*dr*cF*omz);
+		
+		// y0 = sense*radius0*c0;
+		dy0 = sense*(-radius0*s0*omz + dr*c0);
+		ddy0 = sense*(-radius0*c0*omz*omz - 2*dr*s0*omz);
+
+		yF = sense*radiusF*cF;
+		dyF = sense*(-radiusF*sF*omz + dr*cF);
+		ddyF = sense*(-radiusF*cF*omz*omz - 2*dr*sF*omz);
+	}
+
+	struct vec posF = mkvec(xCenter + xF, yCenter + yF, z0 + ascent);
+	struct vec vel0 = mkvec(dx0, dy0, dz);
+	struct vec velF = mkvec(dxF, dyF, dz);
+	struct vec acc0 = mkvec(ddx0, ddy0, 0);
+	struct vec accF = mkvec(ddxF, ddyF, 0);
+	
+	piecewise_plan_7th_order_no_jerk(&p->planned_trajectory, duration,
+		pos0, phi0, vel0, omz, acc0,
+		posF, yawF, velF, omz, accF);
+	
+	p->reversed = false;
+	p->state = TRAJECTORY_STATE_FLYING;
+	p->type = TRAJECTORY_TYPE_PIECEWISE;
+	p->planned_trajectory.t_begin = t;
+	p->trajectory = &p->planned_trajectory;
+	return 0;
+}
+
+int plan_start_trajectory(struct planner *p, struct piecewise_traj* trajectory, bool reversed, bool relative, struct vec start_from)
+{
+	p->reversed = reversed;
+	p->state = TRAJECTORY_STATE_FLYING;
+	p->type = TRAJECTORY_TYPE_PIECEWISE;
+	p->trajectory = trajectory;
+
+	if (relative) {
+		struct traj_eval traj_init;
+		trajectory->shift = vzero();
+		if (reversed) {
+			traj_init = piecewise_eval_reversed(trajectory, trajectory->t_begin);
+		}
+		else {
+			traj_init = piecewise_eval(trajectory, trajectory->t_begin);
+		}
+		struct vec shift_pos = vsub(start_from, traj_init.pos);
+		trajectory->shift = shift_pos;
+	}
+	else {
+		trajectory->shift = vzero();
+	}
+
+	return 0;
+}
+
+int plan_start_compressed_trajectory( struct planner *p, struct piecewise_traj_compressed* trajectory, bool relative, struct vec start_from)
+{
+	p->reversed = 0;
+	p->state = TRAJECTORY_STATE_FLYING;
+	p->type = TRAJECTORY_TYPE_PIECEWISE_COMPRESSED;
+	p->compressed_trajectory = trajectory;
+
+	if (relative) {
+		trajectory->shift = vzero();
+		struct traj_eval traj_init = piecewise_compressed_eval(
+			trajectory, trajectory->t_begin
+		);
+		struct vec shift_pos = vsub(start_from, traj_init.pos);
+		trajectory->shift = shift_pos;
+	} else {
+		trajectory->shift = vzero();
+	}
+
+	return 0;
+}

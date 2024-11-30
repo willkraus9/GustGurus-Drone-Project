46a266eefb3fecc46da86b8aed3a820bf8fea684
DenisAlpay <150480897+DenisAlpay@users.noreply.github.com>
2024-11-30 18:46:24 -0500

    DELETED ALL

--- dynamics/eta_dyn.m
+++
@@ -1,8 +0,0 @@
-function eta_dd = eta_dyn(eta, eta_d, tau)
-%ETA_DYN Summary of this function goes here
-%   Detailed explanation goes here
-    J=inertia_matrix_J(eta,I_B);
-    C = Coriolis(eta,eta_d);
-    eta_dd=inv(J)*(tau-C*eta_d);
-end
-
--- dynamics/inertia_matrix_J.m
+++
@@ -1,9 +0,0 @@
-function J = inertia_matrix_J(eta, I_B)
-    % param eta: roll, pitch, yaw vector
-    % param I_B: Inertia tensor in Body frame
-    % return J: Inertia tensor in world frame
-    W = bigW(eta);
-
-    J = W.T * I_B * W;
-end
-
